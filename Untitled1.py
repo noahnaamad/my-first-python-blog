123661a46dabe562493c487d431d34642e1520e7
noahnaamad <noahnaamad@gmail.com>
2017-07-09 17:24:49 +0000

    practice python functions

---
+++ Untitled1.py
@@ -0,0 +1,13 @@
+# print "hello cruel world"
+#if 3 > 2:
+ #   print("it works!")
+#else:
+ #   print("uh oh")
+    
+# this is a comment
+def hi(names):
+  for name in names:
+      print('hey ' + name + '!')
+      print('whats up')
+      
+hi(['noah', 'hoshi', 'spanky', 'mitzi'])
